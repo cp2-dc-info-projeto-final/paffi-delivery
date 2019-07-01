040c6a6123170067473a0ec15ef44863f55a7e64
Theo <theovini@yahoo.com.br>
2019-07-01 11:37:59 -0300

    cadastro att

--- PHPaffi/pages/home.php
+++ PHPaffi/pages/home.php
@@ -11,7 +11,6 @@ if (!isset($_SESSION['id_usuario'])) {
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
     <link type="text/css" rel="stylesheet" href="../css/materialize.min.css" media="screen,projection" />
     <link type="text/css" rel="stylesheet" href="../css/home.css" media="screen,projection" />
-    <link rel="icon" type="image/png" href="../src/imgs/paffi-logo1.png" sizes="16x16">
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 </head>
 
