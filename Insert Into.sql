ee57a62d684a8df8b217502bc683d7a595b383e7
SinnySin <43237125+SinnySin@users.noreply.github.com>
2019-08-19 09:38:53 -0300

    inserindo Insert Into BD

---
+++ Insert Into.sql
@@ -0,0 +1,14 @@
+INSERT INTO usuario(email, id_usuario, matricula, nome, photoURL, senha, turma)
+VALUES	('julhacp2@gmail.com','1','M02600312','Giulia Fialho Lopes','','julhacpii2','1305')
+;
+INSERT INTO usuario(email, id_usuario, matricula, nome, photoURL, senha, turma)
+VALUES	('sarahferreira252@gmail.com','2','M02100280','Sarah Ferreira Oliveira','','Fer25655','IN301')
+;
+INSERT INTO usuario(email, id_usuario, matricula, nome, photoURL, senha, turma)
+VALUES	('brenasimoes78415@gmail.com','3','M02000783','Brena Alessandra Simões Telles','','brena1812','DS104')
+;
+INSERT INTO usuario(email, id_usuario, matricula, nome, photoURL, senha, turma)
+VALUES	('isabelynhapaulo@gmail.com','4','M02500824','Isabélly da Silva Paulo','','nkd3810ba','1101')
+;
+INSERT INTO usuario(email, id_usuario, matricula, nome, photoURL, senha, turma)
+VALUES	('mariiahangelicah@hotmail.com','5','M02600528 ','Maria Angelica Mauricio da Silva Pereira','','Batatinha19','1201')
