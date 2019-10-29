39be57b258a8aa0596a71e848701f9e2d4d2c778
TheoVinicius <42145513+TheoVinicius@users.noreply.github.com>
2019-10-28 21:37:36 -0300

    Update script.sql

--- script.sql
+++ script.sql
@@ -37,20 +37,16 @@ FOREIGN KEY (id_loja) REFERENCES loja (id_loja)
 
 CREATE TABLE compra (
 id_compra INT AUTO_INCREMENT,
-valor_compra DECIMAL (5,2),
-data_compra DATE,
-hora_compra TIME,
-usuario varchar(30),
-id_produto INT,
+valor_total DECIMAL (5,2),
+timestamp INT,
+id_usuario varchar(30),
 PRIMARY KEY (id_compra),
-FOREIGN KEY(usuario) REFERENCES usuario(id_usuario),
-FOREIGN KEY(id_produto) REFERENCES produto(id_produto)
+FOREIGN KEY(id_usuario) REFERENCES usuario(id_usuario),
 );
 
 CREATE TABLE compra_produto(
 id_produto INT,
 id_compra INT,
-quant INT,
 FOREIGN KEY (id_produto) REFERENCES produto (id_produto),
 FOREIGN KEY (id_compra) REFERENCES compra (id_compra)
 );
